eb43b3da2f8119f68c57115d8236c939ffe2405d
Mark Ziganshin <m.ziganshin.zm@gmail.com>
2024-05-15 20:50:37 +0300

    Fix

--- benchmark/test.sh
+++ benchmark/test.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
+
+# Total time in seconds
+TIMEFMT=%*E
 
 ENCRYPTED_HOST=localhost
 HOST=localhost
@@ -11,6 +14,13 @@ prepare() {
   psql -h $HOST -p 5432 -U postgres -f ../demo_tables.sql $OPTS > /dev/null
 }
 
+reset() {
+  prepare
+  psql -h $HOST -p 5432 -U postgres -f demo_inserts_open.sql $OPTS > /dev/null
+  psql -h $ENCRYPTED_HOST -p 5433 -U postgres -f demo_inserts.sql $OPTS > /dev/null
+}
+
+
 prepare
 
 echo 'Metric 1: unencrypted insert'
@@ -21,23 +31,37 @@ prepare
 echo 'Metric 2: unencrypted insert through the proxy'
 time psql -h $ENCRYPTED_HOST -p 5433 -U postgres -f demo_inserts_open.sql $OPTS > /dev/null
 
+prepare
+
 echo 'Metric 3: encrypted insert'
 time psql -h $ENCRYPTED_HOST -p 5433 -U postgres -f demo_inserts.sql $OPTS > /dev/null
+
+reset
 
 echo 'Metric 4: unencrypted select'
 time psql -h $HOST -p 5432 -U postgres -f select_test_open.sql $OPTS > /dev/null
 
+reset
+
 echo 'Metric 5: unencrypted select through the proxy'
 time psql -h $ENCRYPTED_HOST -p 5433 -U postgres -f select_test_open.sql $OPTS > /dev/null
+
+reset
 
 echo 'Metric 6: encrypted select'
 time psql -h $ENCRYPTED_HOST -p 5433 -U postgres -f select_test.sql $OPTS > /dev/null
 
+reset
+
 echo 'Metric 7: unencrypted join'
 time psql -h $HOST -p 5432 -U postgres -f join_test_open.sql $OPTS > /dev/null
 
+reset
+
 echo 'Metric 8: unencrypted join through the proxy'
-time psql -h $ENCRYPTED_HOST -p 5432 -U postgres -f join_test_open.sql $OPTS > /dev/null
+time psql -h $ENCRYPTED_HOST -p 5433 -U postgres -f join_test_open.sql $OPTS > /dev/null
+
+reset
 
 echo 'Metric 9: encrypted join'
 time psql -h $ENCRYPTED_HOST -p 5433 -U postgres -f join_test.sql $OPTS > /dev/null
--- benchmark_acra/test.sh
+++ benchmark_acra/test.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
+
+TIMEFMT=%*E
 
 ENCRYPTED_HOST=localhost
 HOST=localhost
@@ -11,6 +13,13 @@ prepare() {
   psql -h $HOST -p 5432 -U postgres -f ../demo_tables_acra.sql $OPTS > /dev/null
 }
 
+reset() {
+  prepare
+  psql -h $HOST -p 5432 -U postgres -f demo_inserts_open.sql $OPTS > /dev/null
+  psql -h $ENCRYPTED_HOST -p 5433 -U postgres -f demo_inserts.sql $OPTS > /dev/null
+}
+
+
 prepare
 
 echo 'Metric 1: unencrypted insert'
@@ -21,14 +30,22 @@ prepare
 echo 'Metric 2: unencrypted insert through the proxy'
 time psql -h $ENCRYPTED_HOST -p 9393 -U postgres -f demo_inserts_open.sql $OPTS > /dev/null
 
+prepare
+
 echo 'Metric 3: encrypted insert'
 time psql -h $ENCRYPTED_HOST -p 9393 -U postgres -f demo_inserts.sql $OPTS > /dev/null
+
+reset
 
 echo 'Metric 4: unencrypted select'
 time psql -h $HOST -p 5432 -U postgres -f select_test_open.sql $OPTS > /dev/null
 
+reset
+
 echo 'Metric 5: unencrypted select through the proxy'
 time psql -h $ENCRYPTED_HOST -p 9393 -U postgres -f select_test_open.sql $OPTS > /dev/null
+
+reset
 
 echo 'Metric 6: encrypted select'
 time psql -h $ENCRYPTED_HOST -p 9393 -U postgres -f select_test.sql $OPTS > /dev/null
